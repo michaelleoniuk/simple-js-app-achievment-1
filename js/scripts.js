be4063c058c762451587a2aaaa696114e2f5797e
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-18 12:41:39 +0200

    Errors in code removed

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-  { name: 'Buzz', height: 4, type: 'runner', 'speed'},
-  { name: 'Solo', height: 3, type: 'grass', 'sleep'},
-  { name: 'Vulcano', height: 6, type: 'energy', 'hunting'},
+  { name: 'Buzz', height: '4', type: ('runner', 'speed')},
+  { name: 'Solo', height: '3', type: ('grass', 'sleep')},
+  { name: 'Vulcano', height: '6', type: ('energy', 'hunting')},
 ];
