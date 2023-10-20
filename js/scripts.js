0fbab43fc222cc75a394ea1b536ea9a4b7ee57ea
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-20 12:26:26 +0200

    If else loop added
    I wanted to highlight every pokemon which is >=6 with extra text

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 let pokemonList = [
-  { name: 'Buzz', height: '4', type: ['runner', 'speed']},
-  { name: 'Solo', height: '3', type: ['grass', 'sleep']},
-  { name: 'Vulcano', height: '6', type: ['energy', 'hunting']},
+  { 
+    name: 'Buzz', 
+    height: '4', 
+    type: ['runner', 'speed']
+  },
+  { 
+    name: 'Solo', 
+    height: '3', 
+    type: ['grass', 'sleep']
+  },
+  { 
+    name: 'Vulcano', 
+    height: '6', 
+    type: ['energy', 'hunting']
+  },
   ];
   
   for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >=6) {
+      document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") - Wow, it is huge Pokemon");
+    }else {
   document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
   }
-
+  }
