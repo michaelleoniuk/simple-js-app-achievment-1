37aa468a6e159c3a20ad80a9db0e030574895aab
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-20 11:30:56 +0200

    For loop added

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ let pokemonList = [
   { name: 'Buzz', height: '4', type: ['runner', 'speed']},
   { name: 'Solo', height: '3', type: ['grass', 'sleep']},
   { name: 'Vulcano', height: '6', type: ['energy', 'hunting']},
-];
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
+  }
+
