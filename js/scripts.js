a863be6fa97d646543dc79750dc8d81661263c63
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-20 20:12:56 +0200

    Space added between Pokenoms
    Now Pokemons have spaces between them for better vissibilitz

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,6 @@ let pokemonList = [
     if (pokemonList[i].height >=6) {
       document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") - Wow, it is huge Pokemon");
     }else {
-  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ");
   }
   }
