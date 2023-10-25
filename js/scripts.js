6b3e2e7cedda6e9b38f5922b86e1fbdc3085dcc6
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-26 00:22:40 +0200

    IIFE function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
-let pokemonList = [
-  { 
-    name: 'Buzz', 
-    height: '4', 
-    type: ['runner', 'speed']
-  },
-  { 
+let pokemonRepository = (function () {
+  let pokemonList = [  
+    { 
+     name: 'Buzz', 
+     height: '4', 
+     type: ['runner', 'speed']
+    },
+    { 
     name: 'Solo', 
     height: '3', 
     type: ['grass', 'sleep']
-  },
-  { 
+    },
+    { 
     name: 'Vulcano', 
     height: '6', 
     type: ['energy', 'hunting']
 
-  }];    
+    }];
 
-pokemonList.forEach(function(pokemon) {
+  pokemonList.forEach(function(pokemon) {
   document.write('<p>' + pokemon.name + ' has ' + pokemon.height + ' inches and is described by ' + pokemon.type + '</p>');
-});
+  });
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
