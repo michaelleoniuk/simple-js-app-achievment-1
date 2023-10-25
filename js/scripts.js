0b63d0423dfb08de7a4caf73bb78c073ba71b37c
michaelleoniuk <michael.leoniuk@gmail.com>
2023-10-25 16:54:32 +0200

    Loop type changed from "from" to "forEach"

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,9 @@ let pokemonList = [
     name: 'Vulcano', 
     height: '6', 
     type: ['energy', 'hunting']
-  },
-  ];
-  
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >=6) {
-      document.write("<p>" + pokemonList[i].name + " (Height: " + pokemonList[i].height + ") - Wow, it is a huge Pokemon" + "</p");
-    }else {
-  document.write("<p>" + pokemonList[i].name + " (Height: " + pokemonList[i].height + ") " + "</p>");
-  }
-  }
+
+  }];    
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ' has ' + pokemon.height + ' inches and is described by ' + pokemon.type);
+});
